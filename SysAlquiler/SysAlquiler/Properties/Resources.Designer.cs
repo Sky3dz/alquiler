88cc92f225469d5fedcd4ae123a73007ad859c86
lizethe810 <lizethesl1980@gmail.com>
2019-05-22 15:39:24 -0500

    Diseño formulario documento tipo identidad y modo de pago

--- SysAlquiler/SysAlquiler/Properties/Resources.Designer.cs
+++ SysAlquiler/SysAlquiler/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace SysAlquiler.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap search_1 {
+            get {
+                object obj = ResourceManager.GetObject("search_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
