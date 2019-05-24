9d8545cbace611ac9101bef350b2b14e861e6dd9
Elvis <Elvis@ELVIS>
2019-05-23 23:30:05 -0500

    Formulario Incidencia

---
+++ SysAlquiler/SysAlquiler/Incidencia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysAlquiler
+{
+    public partial class Incidencia : Form
+    {
+        public Incidencia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
